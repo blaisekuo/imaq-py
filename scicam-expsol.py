ecec7485a14aa380bd850abbe4ee51035d234a21
kuotiong <kuotiong@gmail.com>
2020-09-17 16:46:09 +1000

    fix slash n

--- scicam-expsol.py
+++ scicam-expsol.py
@@ -40,7 +40,7 @@ def parse_arguments():
         parser.print_help(sys.stderr)
         sys.exit(1)
 
-    parser.add_argument('-p','--path', type=dir_path, help='path of the raw img files',default='c:\cloudstor\datastore\new_exposures')
+    parser.add_argument('-p','--path', type=dir_path, help='path of the raw img files',default='c:\cloudstor\datastore\\new_exposures')
     parser.add_argument('-n','--name', type=str, help='name prefix of exposures',default='solar')
     parser.add_argument('-s','--shots', type=int, help='number of exposures to take',default=1)
     parser.add_argument('-i','--inttime', type=float, help='integration time',default=0.5)
