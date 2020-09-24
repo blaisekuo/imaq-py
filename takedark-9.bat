e4a8fdaf9db215cc98712ee78a31126d18929e5e
MR Blaise Anthony Kuo Tiong <45027374@students.mq.edu.au>
2020-09-24 16:43:02 +0100

    update bat files

---
+++ takedark-9.bat
@@ -0,0 +1,2 @@
+python scicam-setexp.py -e 9.0
+python scicam-expose.py -t "dark" -n "dark" -r 0 -i 9.0 -m 5 -s 1 -v 50.0
---
+++ takeur-interval.bat
@@ -0,0 +1,2 @@
+python scicam-setexp.py -e 60.0
+python scicam-expose.py -t "calibration" -n "ur" -r 0 -i 60.0 -m 60 -v 3630.0  -s 1
