b4ad85befec39bad6908ca4478996792e8819a14
MR Blaise Anthony Kuo Tiong <45027374@students.mq.edu.au>
2020-09-25 08:28:19 +0100

    update for 20 sec ur exposures

---
+++ takedark-30.bat
@@ -0,0 +1,2 @@
+python scicam-setexp.py -e 30.0
+python scicam-expose.py -t "dark" -n "dark" -r 0 -i 30.0 -m 5 -s 1 -v 150.0 -k -60.0
--- takeur-interval.bat
+++ takeur-interval.bat
@@ -1,2 +1,2 @@
-python scicam-setexp.py -e 60.0
-python scicam-expose.py -t "calibration" -n "ur" -r 0 -i 60.0 -m 60 -v 3600.0  -s 1 -k -60.0
+python scicam-setexp.py -e 20.0
+python scicam-expose.py -t "calibration" -n "ur" -r 0 -i 20.0 -m 3 -v 300.0  -s 60 -k -60.0
