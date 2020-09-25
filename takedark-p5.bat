8d70108b0fcff9a4ed4038f614171fe62e418e30
kuotiong <kuotiong@gmail.com>
2020-09-25 15:36:33 +1000

    update batch files

---
+++ takedark-p5.bat
@@ -0,0 +1,2 @@
+python scicam-setexp.py -e 0.5
+python scicam-expose.py -t "dark" -n "dark" -r 0 -i 0.5 -k -60.0
--- takeflat.bat
+++ takeflat.bat
@@ -1,2 +1,2 @@
 python scicam-setexp.py -e 0.5
-python scicam-expose.py -t "flat" -n "flat" -r 0 -i 0.5 
+python scicam-expose.py -t "flat" -n "flat" -i 0.5 -k -60.0
